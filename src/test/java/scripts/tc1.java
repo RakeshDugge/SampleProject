9f07be74f31b0119f32a36cc7294e3c8cbde66a3
HP <HP@rakesh>
2023-02-12 16:48:43 +0530

    Project

---
+++ src/test/java/scripts/tc1.java
@@ -0,0 +1,14 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class tc1
+{
+	@Test
+	public void demo()
+	{
+		Reporter.log("hi",true);
+	}
+ 
+}
---
+++ src/test/java/scripts/tc2.java
@@ -0,0 +1,14 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class tc2
+{
+	@Test
+	public void demo()
+	{
+		Reporter.log("hello",true);
+	}
+ 
+}
